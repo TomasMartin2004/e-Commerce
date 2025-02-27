c93b5a49b08f3f85c4c1825e97e80a25b473c372
Tomas Martin <tomassmartin04@gmail.com>
2025-02-27 20:29:18 -0300

    fix: deploy

--- backend/ecommerce/ecommerce/urls.py
+++ backend/ecommerce/ecommerce/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('', include('productos.urls')),
     path('', include('custom_auth.urls')),
     path('', include('ventas.urls')),
